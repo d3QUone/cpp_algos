070227191b899e21bf4f1773420a1d7aea26db6d
Vladimir <volkvid@yandex.ru>
2015-02-25 00:51:00 +0300

    project init

---
+++ cpp_algos/cpp_algos/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cpp_algos
+//
+//  Created by Владимир on 25.02.15.
+//  Copyright (c) 2015 Kasatkin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
