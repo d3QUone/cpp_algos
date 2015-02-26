29be523c1593ad18c8ef13224cc36fe3615912fb
Vladimir <volkvid@yandex.ru>
2015-02-26 22:12:17 +0300

    Power function (lect 1)

--- cpp_algos/cpp_algos/main.cpp
+++ cpp_algos/cpp_algos/main.cpp
@@ -8,8 +8,23 @@
 
 #include <iostream>
 
+// returns a^n
+double Power(double a, int n) {
+    double result = 1.0;
+    double aInDegreeOf2 = a;
+    while (n > 0) {
+        if ((n & 1) == 1) {
+            result *= aInDegreeOf2;
+        }
+        aInDegreeOf2 *= aInDegreeOf2;
+        n = n >> 1;
+    }
+    return result;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    //std::cout << "Hello, World!\n";
+    
+    printf("%.1f\n", Power(2, 100));
     return 0;
 }
