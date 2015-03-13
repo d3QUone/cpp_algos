7024c9aa76d8ea005ddea67d0c7ede4ca1be954a
Vladimir <volkvid@yandex.ru>
2015-03-13 16:14:38 +0300

    evaluate RPN - OK. transform now

--- c_B3_parser/c_B3_parser/main.c
+++ c_B3_parser/c_B3_parser/main.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 /*
@@ -25,11 +26,8 @@
 #define MAX_LEN 1024
 
 
-// add simple stack here?...
-
-
-
-size_t operators(char to_check) {
+// check if current char is operator
+size_t is_operator(char to_check) {
     if (to_check == '+' ||
         to_check == '-' ||
         to_check == '*' ||
@@ -40,19 +38,83 @@ size_t operators(char to_check) {
 }
 
 
-// Reverse Polish Notation - processor
-double RPN(char* exp, size_t len){
-    double result = 0.0;
+// evaulates array in Postfix
+double evaulate_rpn(char** exp, size_t len) {
+    double* stack = (double* ) malloc(len*sizeof(double)); // stack for operands
+    size_t place = 0;
     
-    // .1234567890+-*/()
+    // used to process sub eval
+    double a1 = 0.0;
+    double a2 = 0.0;
     
-    // - check if char is 'enabled' -> parse doubles
+    for (size_t i = 0; i < len; ++i) {
+        printf("%s\n", exp[i]);
+        
+        if ( is_operator(*exp[i]) == 1) {
+            // takes out last 2 operands, processes
+            if (place > 1) {
+                a1 = stack[place - 2];
+                a2 = stack[place - 1];
+                
+                if (strncmp(exp[i], "+", 1) == 0)
+                    stack[place - 2] = a1 + a2;
+                else if (strncmp(exp[i], "-", 1) == 0)
+                    stack[place - 2] = a1 - a2;
+                else if (strncmp(exp[i], "*", 1) == 0)
+                    stack[place - 2] = a1 * a2;
+                else if (strncmp(exp[i], "/", 1) == 0)
+                    stack[place - 2] = a1 / a2;
+                
+                place -= 1;
+            } else {
+                // wrong order / anything else
+                printf("[error]");
+                exit(1);
+            }
+        } else {
+            // is operand -> convert char to double -> save double to stack
+            stack[place] = atof(exp[i]);
+            place++ ;
+        }
+    }
+    return stack[0]; // result here
+}
+
+
+
+// transform from Infix to Postfix notation
+double create_rpn(char* exp, size_t len){
+    char buffer; // one current char
+    //char* doubles = (char* ) malloc(len*sizeof(char)); // to convert string to double
     
-    // - find the deepest sub-expression
+    char** stack = (char** ) malloc(len*sizeof(char* )); // stack for numbers, operators
     
-    // - eval in recursion?
+    size_t index = 0;
+    size_t top = 0;  // position of last item in stack
+    //size_t flag = 0; // used to separate digits
     
-    return result;
+    while (index < len) {
+        buffer = exp[index]; //printf("%c\n", buffer);
+        
+        if (buffer == '(' || buffer == ')' ) {
+            // pull out of stack?
+            
+        } else {
+            
+            /*if ( is_operator(buffer) == 1 ) {
+                // operator...
+                
+            } else {
+             
+             // operand!
+             
+             // parse doubles
+             
+            }*/
+        }
+        index++ ;
+    }
+    return evaulate_rpn(stack, top);
 }
 
 
@@ -72,7 +134,12 @@ int main(int argc, const char * argv[]) {
             
             // can add resizing later here!
         }
-        double result = RPN(expression, len);
+        //double result = create_rpn(expression, len);
+        
+        //char* test[] = {"1", "2", "+", "3", "4", "-", "+"}; // 2.00
+        //char* test[] = {"10.3", "2", "+", "3", "4", "+", "+"}; // 19.30
+        char* test[] = {"11", "3", "9", "*", "+"}; // 38.00
+        double result = evaulate_rpn(test, sizeof(test)/sizeof(char* ));
         printf("%.2f", result);
         free(expression);
     }
