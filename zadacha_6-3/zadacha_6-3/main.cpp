8af8280b3952d0415fe432b23b8903b5df097cee
Vladimir <volkvid@yandex.ru>
2015-03-21 00:21:15 +0300

    improves, fixed deleting

--- zadacha_6-3/zadacha_6-3/main.cpp
+++ zadacha_6-3/zadacha_6-3/main.cpp
@@ -135,17 +135,11 @@ void SiftUp(int* arr, int i) {
 }
 
 
-void Add(int* arr, int size, int item) {
-    arr[size++] = item;
-    SiftUp(arr, size - 1); // sift up from its current place
-}
-
-
 int ExtractMin(int* arr, int size) {
     assert(size >= 0);
     int result = arr[0];
     
-    arr[0] = arr[size-- ];
+    arr[0] = arr[--size];
     if (size > 0) {
         SiftDown(arr, size, 0);
     }
@@ -161,47 +155,51 @@ int ExtractMin(int* arr, int size) {
 
 int min_routes(int* time_in, int* time_out, int n){
     int* heap = new int[n];
-    int size = 0; // num of current fulled ro
-    int real_size = 0;
+    int size = 0; // num of current fulled routes
+    int max_size = 0;
     
     int current_depart_time = 0;
     int current_arrive_time = 0;
     int min_time = 0;
     
+    int debug = 0;
+    
     for (int i = 0; i < n; ++i) {
-        std::cout << "------------------------\n";
+        if (debug == 1)
+            std::cout << "----------------------------------------\n";
         
         current_arrive_time = time_in[i];
         current_depart_time = time_out[i];
         
-        // min at root
         if (size > 0) {
-            min_time = heap[0];
+            min_time = heap[0]; // don't delete on checkup
             std::cout << "Min time now = " << min_time << "\n";
-            
-            while (min_time < current_arrive_time && size > 0){
+            while (min_time < current_arrive_time && size > 0) {
                 min_time = ExtractMin(heap, size--);
+                std::cout << "--min time now = " << min_time << "; size = " << size << "\n";
             }
         }
         
-        // add new item in heap
+        // Add new item in heap
         heap[size] = current_depart_time;
         SiftUp(heap, size);
         size++ ;
         
-        // debug print
-        std::cout << "heap: ";
-        for (int j = 0; j < size; ++j) {
-            std::cout << heap[j] << " ";
+        // max size of tree
+        if (size > max_size) {
+            max_size = size;
         }
-        std::cout << "\n";
         
-        
-        if (size > real_size) {
-            real_size = size;
+        if (debug == 1) {
+            std::cout << "heap: ";
+            for (int j = 0; j < size; ++j) {
+                std::cout << heap[j] << " ";
+            }
+            std::cout << "\n";
         }
     }
-    return real_size;
+    delete [] heap;
+    return max_size;
 }
 
 
@@ -209,12 +207,18 @@ int main(){
     int n = 0;
     std::cin >> n;
     
-    int heap_time_in[n]; // прибытие
-    int heap_time_out[n]; // отбытие
+    int* heap_time_in = new int[n]; // прибытие
+    int* heap_time_out = new int[n]; // отбытие
     for (int i = 0; i < n; ++i) {
         std::cin >> heap_time_in[i] >> heap_time_out[i];
+        //std::cout << i << "\n";
     }
+    //std::cout << "Got " << n << "\n";
     
     int res = min_routes(heap_time_in, heap_time_out, n);
-    std::cout << res;
+    std::cout << res << "\n";
+    
+    delete [] heap_time_in;
+    delete [] heap_time_out;
+    return 0;
 }
