9a7a9de637d0ea7e5f2b13b9a4fa3ea22c25b341
Vladimir <volkvid@yandex.ru>
2015-04-01 23:03:14 +0300

    no message

---
+++ sort_2_3/sort_2_3/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  sort_2_3
+//
+//  Created by Владимир on 01.04.15.
+//  Copyright (c) 2015 Kasatkin. All rights reserved.
+//
+
+#include <iostream>
+#include <stdio.h>
+#include <vector>
+
+
+
+void sort(){
+    
+    
+}
