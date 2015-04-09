a522cd1d65f5db207368685c4c684bcff1165d6e
Vladimir <volkvid@yandex.ru>
2015-04-09 23:59:27 +0300

    no message

--- sort_2_3/sort_2_3/main.cpp
+++ sort_2_3/sort_2_3/main.cpp
@@ -49,22 +49,42 @@ bool angle(const CPoint& L, const CPoint& R){
     return get_sin(L) < get_sin(R);
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+template <class T>
+void sift_up(T* arr, int len, bool (*is_less)(const T&, const T&)){
+    
+}
+
+template <class T>
+void sift_down(T* arr, int len, bool (*is_less)(const T&, const T&)){
+    
+}
+
+template <class T>
+void sort(T* arr, int len, bool (*is_less)(const T&, const T&)){
+    
+}
+*/
 
 // insert 'x' in heap
 // n - current heap size
 template <class T>
 void heap_insert(T* arr, int n, T x, bool (*is_less)(const T&, const T&)){
+    // insert
     arr[n+1] = x;
+
+    // do sift-up
     for (int i = n + 1; i > 1; --i) {
         if (is_less(arr[i/2], arr[i])) {
             std::swap(arr[i], arr[i/2]);
-            i = i/2;
+            i /= 2;
         } else {
             break;
         }
     }
 }
-
 
 
 template <class T>
@@ -80,44 +100,6 @@ void heap_pop(T* arr, int n, bool (*is_less)(const T&, const T&)){
         }
     }
 }
-
-
-template <class T>
-void heap_sort(T* arr, int n, bool (*is_less)(const T&, const T&)) {
-    T* buf = new T[n+1];
-    for (int i = 0; i < n; ++i) {
-        heap_insert(buf, i, arr[i], is_less);
-    }
-    for (int i = 0; i < n; ++i) {
-        arr[n-1-i] = buf[1];
-        heap_pop(buf, n-i, is_less);
-    }
-}
-
-
-/// fixing heap sort ///
-
-bool int_is_less(const int& L, const int& R){
-    return L < R;
-}
-
-int main(){
-    int n = 0;
-    int* arr = new int[n];
-    std::cin >> n;
-    for (int i = 0; i < n; ++i) {
-        std::cin >> arr[i];
-    }
-    
-    heap_sort<int>(arr, n, int_is_less);
-    for (int i = 0; i < n; ++i) {
-        std::cout << arr[i] << " ";
-    }
-    
-    return 0;
-}
-
-///////////////////////////
 
 
 template <class T>
@@ -141,8 +123,64 @@ void make_heap(T* arr, int n, bool (*is_less)(const T&, const T&)) {
 }
 
 
+template <class T>
+void heap_sort(T* arr, int n, bool (*is_less)(const T&, const T&), void present(T* arr, int n)) {
+    make_heap(arr, n, is_less); /// <- fix!
+    present(arr, n);
+    
+    T* buf = new T[n+1];
+    for (int i = 0; i < n; ++i) {
+        heap_insert(buf, i, arr[i], is_less);
+        
+        std::cout << "Insert: ";
+        present(buf, n+1);
+    }
+    for (int i = 0; i < n; ++i) {
+        arr[n-1-i] = buf[1];
+        heap_pop(buf, n-i, is_less);
+        
+        //std::cout << "\nPop " << arr[n-1-i] << " -> " << n-i-1 << "\n";
+        std::cout << "\nPop\n";
+        present(buf, n+1);
+        present(arr, n);
+    }
+}
 
-// issue with first item
+
+template <class T>
+void heap_sort(T* arr, int n, bool (*is_less)(const T&, const T&)) {
+    make_heap(arr, n, is_less); /// <- fix!
+    
+    T* buf = new T[n+1];
+    for (int i = 0; i < n; ++i) {
+        heap_insert(buf, i, arr[i], is_less);
+    }
+    for (int i = 0; i < n; ++i) {
+        arr[n-1-i] = buf[1];
+        heap_pop(buf, n-i, is_less);
+    }
+}
+
+
+bool int_is_less(const int& L, const int& R){
+    return L < R;
+}
+
+void present_i(int* arr, int n) {
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << "\n";
+}
+
+void present_s(CPoint* arr, int n) {
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i].x << " " << arr[i].y << "\n";
+    }
+    std::cout << "\n";
+}
+
+
 template <class T>
 void quick_heap_sort(T* arr, int n, bool (*is_less)(const T&, const T&)) {
     make_heap(arr - 1, n, is_less);
@@ -151,11 +189,27 @@ void quick_heap_sort(T* arr, int n, bool (*is_less)(const T&, const T&)) {
     }
 }
 
+int mainxx(){
+    int n = 0;
+    int* arr = new int[n];
+    std::cin >> n;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> arr[i];
+    }
+    
+    heap_sort<int>(arr, n, int_is_less, present_i);
+    //quick_heap_sort<int>(arr, n, int_is_less);
+    
+    std::cout << "Result: ";
+    for (int i = 0; i < n; ++i) {
+        std::cout << arr[i] << " ";
+    }
+    return 0;
+}
 
-//template <class T>
-//void sift_down(T* arr, int k, int n, bool (*is_less)(const T&, const T&)) {}
+////////////////////////////////////////////////////////////////////////////////////////////
 
-int main_p(){
+int main(){
     int n = 0;
     int min_point = 0;
     std::cin >> n;
@@ -173,10 +227,6 @@ int main_p(){
         }
     }
     
-//    for (int i = 0; i < n; ++i) {
-//        std::cout << arr[i].x << " " << arr[i].y << "\n";
-//    } std::cout << "----\n";
-    
     // delete dat base-point from sorting
     CPoint::SetBase(min.x, min.y);
     CPoint* new_arr = new CPoint[n-1];
@@ -189,7 +239,7 @@ int main_p(){
     }
     delete [] arr;
     
-    heap_sort<CPoint>(new_arr, n-1, angle);
+    heap_sort<CPoint>(new_arr, n-1, angle, present_s);
     std::cout << CPoint::baseX << " " << CPoint::baseY << "\n";
     for (int i = 0; i < n-1; ++i) {
         std::cout << new_arr[i].x << " " << new_arr[i].y << "\n";
